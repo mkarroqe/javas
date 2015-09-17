3438c80a4e3e5d2b52b48005642338e834278b0c
Mary Karroqe <mkarroqe@gmail.com>
2015-09-17 19:42:32 -0400

    Update Input5_1.java
    commented out buggy code

--- Input5_1/Input5_1.java
+++ Input5_1/Input5_1.java
@@ -21,7 +21,7 @@ public class Input5_1 {
 
 		int age = myScanner.nextInt(); // another built in method for reading ints; nextDouble is also a thing
 		int nextYearAge = age + 1;
-
+/* debugging this
 		if (nextYearAge == 18) {
 			String extra = "You will be your own responsible and independent adult.";
 		}
@@ -31,9 +31,9 @@ public class Input5_1 {
 		else {
 			String extra = "Congrats.";
 		}
-		
+*/		
 		System.out.println("Next year you will be " + nextYearAge + ". ");
-		System.out.print(extra);
+//		System.out.print(extra);
 
     }
 }
