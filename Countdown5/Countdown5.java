b476586bb571ee744367131228087374d128b080
mkarroqe@gmail.com <mkarroqe@gmail.com>
2015-09-22 12:04:54 -0400

    updated with today’s classwork, readme coming soon

--- Countdown5/Countdown5.java
+++ Countdown5/Countdown5.java
@@ -1,22 +1,56 @@
 // Mary Karroqe PD 4/5
 
 public class Countdown5 {
+
+	private static final int DEFAULT_START = 10;
+	private static final int DEFAULT_STEP = 2;
+
 	public static void main(String[] args){
-		countdown();
+
+		double rand = Math.random();
+    	int randStart = (int)(rand*100) + 1;
+    	int randStep = (int)(rand*randStart) + 1;
+
+		countdown(); // will adhere to DEFAULT_START and DEFAULT_STEP
+		countdown(6); // will adhere to DEFAULT_STEP
+		countdown(randStart, randStep); // will do whatever it wants ;)
 	}
 
 	public static void countdown(){
-		int num = 10;
 
-		while (num >= 0){
-			System.out.println(num);
-			num--;
-		}
-		System.out.println("BLAST OFF!");
+		countdown(DEFAULT_START);
+
 	}
 
 	public static void countdown(int start){
-		// formal parameter
-		// whatever that int is, that's the start number
+		countdown(start, DEFAULT_STEP);
+	}
+
+	public static void countdown(int start, int step){
+		step = Math.abs(step); //always a positive number
+
+		int num = start;
+		while (num > 0){
+			System.out.print(num + "...");
+			num = num - step;
+
+			long time1 = System.currentTimeMillis();
+			while (System.currentTimeMillis() - time1 < 1000) {
+				// keep me in a loop for one second (1000 ms) to create a delay
+			}
+
+		}
+		System.out.println("BLAST OFF!");
+
 	}
 }
+
+	/* NOTES: OVERLOADING A METHOD:
+
+		 helping w not repeating code
+		 static vs nonstatic- static is a stand alone method
+	     Scanner is not static, it builds from the Scanner object
+		 Math.random is static, call it by class name, doesn't require another object
+		 void - the method does not return anything
+
+		*/
--- Countdown5/README.md
+++
@@ -1,3 +0,0 @@
-#Countdown5
-Counting down from 10 to 0 to blast off.
-Quick demonstration on how to create a method and call it from the main method.
