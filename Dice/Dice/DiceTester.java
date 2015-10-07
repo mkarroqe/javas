7526d80df2944aa8064a143a51ea80863051ff63
Mary Karroqe <mkarroqe@gmail.com>
2015-10-07 17:44:41 -0400

    update

--- Dice/Dice/DiceTester.java
+++ Dice/Dice/DiceTester.java
@@ -1,14 +1,29 @@
 // Mary Karroqe PD 4/5
 
-public class DiceTester {
-	public static void main(String[] args) {
-		NumberCube myCube = new NumberCube(); // crating new instance of the NumberCube object
+public class DiceTester
+{
+	public static void main(String[] args)
+	{
+		NumberCube myCube = new NumberCube(); // creating new instance of the NumberCube object w default values
+		NumberCube crazyCube = new NumberCube(93);
 
+		// default NumberCube
 		int numOfRolls = 0;
 		int theRoll;
-
-		while (numOfRolls < 10) {
+		while (numOfRolls < 10)
+		{
 			theRoll = myCube.roll();
+			System.out.println(theRoll);
+			numOfRolls++;
+		}
+
+		System.out.println();
+
+		// crazyCube, 93 sides
+		numOfRolls = 0;
+		while (numOfRolls < 10)
+		{
+			theRoll = crazyCube.roll();
 			System.out.println(theRoll);
 			numOfRolls++;
 		}
--- Dice/Dice/NumberCube.java
+++ Dice/Dice/NumberCube.java
@@ -1,14 +1,37 @@
 // Mary Karroqe Pd 4/5
+// instructions for any number cube
 
-public class NumberCube {
-	private int numOfSides; // field/instance variable; gets default value
+public class NumberCube
+{
+	private int numOfSides; // field/instance variable; gets default value (local variables do not) of 0
+	public static final int MINIMUM_SIDES = 4; // final declares it as a constant
+	public static final int MAXIMUM_SIDES = 32;
+	public static final int DEFAULT_SIDES = 6;
 
-	// constructors do not have a return type
-	public NumberCube() {
-		numOfSides = 6; // anytime that a new NumberCube is built w no parameters, this is the default
+	// constructors do not have a return type! instead it returns the instance of that class (aka new NumberCube)
+	public NumberCube()
+	{
+		// if no parameter is given:
+		numOfSides = DEFAULT_SIDES;
 	}
 
-	public int roll() {
+	// overloading the constructor
+	public NumberCube(int sides)
+	{
+		// if you pass in a parameter, sides:
+		numOfSides = sides;
+	}
+
+	// our "verb"
+	public int roll()
+	{
 		return ((int)(Math.random() * numOfSides) + 1);
 	}
 }
+
+/* something static: owned by class, not object
+ * our constants are static because it's something that all the individual number cubes can share
+ *
+ *
+ *
+ */
